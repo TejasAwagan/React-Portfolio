ef91999426dd57d6bcc02ea2bcf671476bff8c8d
Tejas Awagan <tejas.awagan29@gmail.com>
2024-10-21 21:42:59 +0530

    Info updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,6 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -9,47 +8,40 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
+    year: "07 June 2023 - 30 July 2023",
+    role: "Web Developer",
+    company: "Jolle",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    technologies: ["Javascript", "React.js", "Rest API", "mongoDB", "Nodejs", "Expressjs"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "BookItNow ( Movie Ticket Booking System )",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Developed a comprehensive movie ticket booking system with roles for both Admin and Local Users. Features included ticket booking, email confirmations, and password reset for users. The project implemented a secure authentication system and a responsive UI, ensuring optimal user experience across devices. MongoDB was used for data storage, with robust validation and security protocols.",
+    technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "Redux", "JWT Token"],
   },
   {
-    title: "Task Management App",
+    title: "SignIn and SignUp Form",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Created a user authentication system with error handling and validation to enhance security and user experience. MongoDB was integrated to store and manage user credentials, ensuring efficient data retrieval and storage.",
+    technologies: ["React.js", "Node.js", "MongoDB", 'Express', "JWT Token"],
   },
   {
-    title: "Portfolio Website",
+    title: "Matching Cards Game",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+      "Developed a memory matching game with essential features such as a dynamic grid layout, card flipping, timer functionality, and move counter. The project included user registration for engagement and win/lose conditions to provide a structured gaming experience​",
+    technologies: ["React.js", 'HTML', "CSS", "JavaScript"],
+  }
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Aundh, Pune - 411007",
+  phoneNo: "+91 7218292094",
+  email: "tejas.awagan29@gmail.com",
 };
