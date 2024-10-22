a9bae09f1d0972c5e9052f55f7965d8449fb5864
Tejas Awagan <tejas.awagan29@gmail.com>
2024-10-22 12:15:10 +0530

    components updated

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,20 +8,25 @@ const Experience = () => {
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
-        className="my-20 text-center text-4xl"
+        className="my-10 text-center text-2xl md:text-3xl lg:text-4xl"
       >
         Experience
       </motion.h1>
       <div>
         {EXPERIENCES.map((experience, index) => (
-          <div key={index} className="mb-8 flex lg:justify-center">
+          <div
+            key={index}
+            className="pl-32 mb-2 flex flex-wrap lg:justify-center"
+          >
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 0.5 }}
-              className="w-full lg:w-1/4"
+              className="w-full lg:w-1/3 mb-4 lg:mb-0"
             >
-              <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
+              <p className="mb-2 text-sm text-neutral-400 text-center lg:text-left">
+                {experience.year}
+              </p>
             </motion.div>
 
             <motion.div
@@ -30,21 +35,25 @@ const Experience = () => {
               transition={{ duration: 0.5 }}
               className="w-full max-w-xl lg:w-3/4"
             >
-              <h6 className="mb-2 font-semibold">
+              <h6 className="mb-2 font-semibold text-center lg:text-left">
                 {experience.role} -{" "}
                 <span className="text-sm text-purple-200">
                   {experience.company}
                 </span>
               </h6>
-              <p className="mb-4 text-neutral-400">{experience.description}</p>
-              {experience.technologies.map((tech, index) => (
-                <span
-                  key={index}
-                  className="mr-2 mt-4 rounded bg-neutral-800 px-2 py-1 text-sm font-medium text-purple-800"
-                >
-                  {tech}
-                </span>
-              ))}
+              <p className="mb-4 text-neutral-400 text-center lg:text-left">
+                {experience.description}
+              </p>
+              <div className="flex flex-wrap justify-center lg:justify-start">
+                {experience.technologies.map((tech, index) => (
+                  <span
+                    key={index}
+                    className="mr-2 mt-4 rounded bg-neutral-800 px-2 py-1 text-sm font-medium text-purple-800"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
             </motion.div>
           </div>
         ))}
